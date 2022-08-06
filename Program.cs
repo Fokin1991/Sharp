88cbaa8027c35a08f36c87f55725b8223c3c3d54
Ivan <soldren2@gmail.com>
2022-08-06 22:36:52 +0300

    base

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿Console.WriteLine("Введите имя пользователя");
-string username = Console.ReadLine();
-if (username.ToLower() =="маша")
-{
-    Console.WriteLine ("Ура это же Маша");
-}
-else
-{
-    Console.Write ("Привет, ");
-    Console.WriteLine (username);
-}
+﻿int a =1;
+int b =2;
+int c = 3;
+int d = 4;
+int e = 8;
+
+int max=a;
+
+if(a>max) max=a;
+
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+Console.Write(max);
+
+
