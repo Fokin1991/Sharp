1c2a5f8abd736d922447b914ca82c187b63db311
Ivan <soldren2@gmail.com>
2022-08-06 22:12:29 +0300

    if/else

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿Double NumberA=12;
-Double NumberB=5;
-Console.WriteLine(NumberA/NumberB);
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() =="маша")
+{
+    Console.WriteLine ("Ура это же Маша");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
